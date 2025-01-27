d499008827b15dae4ed913abde18eea32f416740
vikasgamersback <vikas@gamersback.com>
2025-01-27 15:48:46 +0530

    routes created

--- backend_class/routes/signupLoginRoutes.js
+++ backend_class/routes/signupLoginRoutes.js
@@ -8,8 +8,6 @@ router.post('/signup', (req, res) => authController.signup(req, res));
 
 router.post('/login', (req, res) => authController.login(req, res));
 
-router.post('/signup', (req, res) => authController.signup(req, res));
-
 router.post('/block/:uid', (req, res) => authController.blockUser(req, res));
 router.post('/unblock/:uid', (req, res) => authController.unblockUser(req, res));
 
